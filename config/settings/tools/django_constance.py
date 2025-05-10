3044c679467e44c774ff26203c60621ef70a0ce1
Robert Arzola Castillo <RobertArzolaC>
2025-05-10 15:45:15 -0500

    feat: Actualizar la gestión de logos en la interfaz y agregar soporte para logo oscuro

--- config/settings/tools/django_constance.py
+++ config/settings/tools/django_constance.py
@@ -13,7 +13,8 @@ CONSTANCE_ADDITIONAL_FIELDS = {
 }
 
 CONSTANCE_CONFIG = {
-    "LOGO_IMAGE": ("default.png", _("Company logo"), "image_field"),
+    "LOGO_IMAGE": ("default.png", _("Company default logo"), "image_field"),
+    "DARK_LOGO_IMAGE": ("dark_logo.png", _("Company dark logo"), "image_field"),
     "JSON_FIELD_TEST": ({"name": "Robert"}, _("Test json field"), "json_field"),
     "ENABLE_SEND_EMAIL": (True, _("Enable sending emails.")),
     "ENABLE_VERIFICATION_EMAIL": (True, _("Enable email verification.")),
@@ -24,6 +25,7 @@ CONSTANCE_CONFIG_FIELDSETS = {
     "1. General Options": {
         "fields": (
             "LOGO_IMAGE",
+            "DARK_LOGO_IMAGE",
             "JSON_FIELD_TEST",
             "WEBSITE_DOMAIN",
             "ENABLE_SEND_EMAIL",
